bf36b6a6a6b5f80d835886a4b715e11d6b92f17c
devpresss xmont <xmontemorjerald@gmail.com>
2026-05-17 20:28:54 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h3 className='text-2xl text-teal-500 text-center'>
+          First React Project</h3>
+      
+    </>
+  )
+}
+
+export default App
